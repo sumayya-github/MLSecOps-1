704b135305c4594cb00b27dc1afb2d6ffd09e8ba
sumayya-github <66043790+sumayya-github@users.noreply.github.com>
2020-07-02 14:28:23 +0530

    httpd first commit

--- httpd_convert.py
+++ httpd_convert.py
@@ -1,4 +1,6 @@
 import re
+import numpy as np
+import pandas as pd
 def extract(filename):
     with open(filename) as f:
         log = f.read()
@@ -6,10 +8,8 @@ def extract(filename):
         ips_list = re.findall(regexp2, log)
         return ips_list
 logs = extract('/var/log/httpd/access_log')
-import numpy as np
 log_arr = np.array(logs)
 ip=log_arr[:,0]
-import pandas as pd
 dataset = pd.DataFrame({'IP': log_arr[:, 0], 'A': log_arr[:, 1],'B':log_arr[:, 2],'Date&Time':log_arr[:, 3],'TZ':log_arr[:, 4],'C':log_arr[:, 5],'Site':log_arr[:, 6],'Protocol':log_arr[:, 7],'Status':log_arr[:, 8],'Length':log_arr[:, 9]})
 print(dataset.head())
-dataset.to_csv('weblog.csv')
+dataset.to_csv('httpdlog.csv')
